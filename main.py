596493d99fe4981e9a67c42acd035241c3be6d92
AAA <57193966+vzcz@users.noreply.github.com>
2022-05-10 20:22:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -55,22 +55,70 @@ async def on_message(message):
 
 
     if userList[0].lower() == '!image':
-        imageGen = message.content[7:].replace(" ", "%20")
-        api_url = f'https://api.pexels.com/v1/search?query={imageGen}&per_page=5&page=1'
-        print(imageGen)
+        numImage = 5
+        try:
+            lenNum = len(userList[1])
+            numImage = int(userList[1])
+            imageGen = message.content[8 + lenNum:].replace(" ", "%20")
+            api_url = f'https://api.pexels.com/v1/search?query={imageGen}&per_page={numImage}&page=1'
+        except:
+            imageGen = message.content[7:].replace(" ", "%20")
+            api_url = f'https://api.pexels.com/v1/search?query={imageGen}&per_page=5&page=1'
         header = {'Content-Type': 'application/json',
                   'Authorization': api_token}
 
         response = requests.get(api_url, headers=header)
 
         jsontest = response.json()
-        print(jsontest)
 
-        for i in range(4):
+        for i in range(numImage):
             await message.channel.send(jsontest['photos'][i]['src']['large'])
 
+    if user_message == '!!help':
+        embed = discord.Embed(
+            colour= discord.Colour.random()
+        )
+        embed.set_author(name="Help")
+        embed.add_field(name='!random <select> <select> <select> <select>', value="Random selection\nEx: !random ali ahmad saad khalid", inline=False)
+        embed.add_field(name='!image <number of images> <image name>', value='Gives you a set of photos (the number of photos is optional)\nEx: !image 2 sky', inline=False)
+        embed.add_field(name='!proxy <type>', value='Generate free proxy for different types \ntypes:\nhttp\nsock4\nsock5\nEx: !proxy http', inline=False)
+        await message.channel.send(embed=embed)
+
+    if userList[0].lower() == '!proxy':
+        try:
+            # Choose the type of proxy
+            if userList[1].lower() == "socks5":
+                prx_url = "https://api.proxyscrape.com/v2/?request=getproxies&protocol=socks5&timeout=10000&country=all"
+                typePrx = "socks5"
+            elif userList[1].lower() == "socks4":
+                prx_url = "https://api.proxyscrape.com/v2/?request=getproxies&protocol=socks4&timeout=10000&country=all"
+                typePrx = "socks4"
+            elif userList[1].lower() == "http":
+                prx_url = "https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all"
+                typePrx = "HTTP"
+
+            res = requests.get(prx_url)
+
+            #
+            if "502 Bad Gateway" in res.text or "Error 543" in res.text:
+                await message.channel.send("Wait a moment, then try again there are some problems")
+            else:
+                await message.channel.send(typePrx)
+                filePrx = open("resprox.txt", "w")
+                result = res.text.replace("\n", " ")
+                filePrx.write(result)
+                filePrx.close()
+                await message.channel.send(file=discord.File('resprox.txt'))
 
 
+        except:
+
+            if len(userList) == 1:
+                await message.channel.send("Please enter proxy type")
+            elif userList[1].lower() != "http" or userList[1].lower() != "socks5" or userList[1].lower() != "socks4":
+                await message.channel.send("Incorrect type")
+            else:
+                await message.channel.send("Wait a minute, there are some problems")
 
 
 client.run(Token)
